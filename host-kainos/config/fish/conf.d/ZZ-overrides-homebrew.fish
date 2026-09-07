21374246fbc97445c72b5bb6f08a0c786c6a85de
Christian Sutter <git@doublesignal.com>
2026-09-07 15:46:09 +0100

    Make sure Kainos machine can actually use fish

--- host-kainos/config/fish/conf.d/ZZ-overrides-homebrew.fish
+++ host-kainos/config/fish/conf.d/ZZ-overrides-homebrew.fish
@@ -3,3 +3,6 @@ set -gx HOMEBREW_CASK_OPTS "--appdir=/Users/$USER/Applications"
 
 # Skip installing casks that don't work on this machine
 set -gx HOMEBREW_BUNDLE_CASK_SKIP "linearmouse"
+
+# Exec-ing fish through .zprofile doesn't seem to set $SHELL properly, so set it again
+set -gx SHELL (command -v fish)
